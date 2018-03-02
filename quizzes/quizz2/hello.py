ea68e9f26288da2a8f7b6afc05832f3bc66a79d6
venkatpushpak <venkat.pushpak5@gmail.com>
2018-03-01 17:28:01 -0800

    Update hello.py

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -1,13 +1,41 @@
-from flask import Flask
-from flask import request
 
-app = Flask(__name__)
+from flask import Flask,request,Response
+from flask import jsonify
+app=Flask(__name__)
+global dict1
+dict1 ={}
 
-@app.route('/', methods=['GET'])
+
+@app.route("/")
 def hello():
-    return "Hello World!"
+    
+    return "hello world"
+ 
 
-@app.route('/users', methods=['POST'])
-def new_users():
-    name = request.form["name"]
-    return "Hello {}!".format(name)
+@app.route('/users',methods=['POST','GET'])
+def get1():
+    if request.method=='POST':
+        #dict1={}
+        dict1["id"]=1
+        dict1["name"]=request.form["name"]
+        #dict1.save()
+        return jsonify(dict1) 
+@app.route('/users/<id1>',methods=['GET','DELETE'])
+def get2(id1):
+    if request.method=='GET':
+        if dict1["id"]==int(id1):
+            return jsonify(dict1)
+    if  request.method=='DELETE':
+         if dict1["id"]==int(id1):
+             del dict1["id"]
+             del dict1["name"]
+             return jsonify(dict1)      
+        
+    
+
+
+
+
+
+        
+
